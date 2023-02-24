e452b0170b9e8f774bfb055c0974aa1287c4e7dc
Sapitorico <renzoyaques@gmail.com>
2023-02-24 17:09:20 -0300

    tests from class filestorage

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Tests form  FileStorage class"""
+import unittest
+from models.engine.file_storage import FileStorage
+from models import storage
+
+
+
+class TestFileStorage(unittest.TestCase):
+    def test_init(self):
+        self.assertTrue(hasattr(FileStorage, "__init__"))
+        self.assertTrue(hasattr(FileStorage, "all"))
+        self.assertTrue(hasattr(FileStorage, "new"))
+        self.assertTrue(hasattr(FileStorage, "save"))
+        self.assertTrue(hasattr(FileStorage, "reload"))
+
+    def test_all(self):
+        self.assertTrue(hasattr(storage, "all"))
+        self.assertEqual(type(storage.all()), dict)
+        self.assertTrue(hasattr(storage, "all"))
+        self.assertEqual(type(storage.all()), dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
