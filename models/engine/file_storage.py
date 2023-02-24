16bb62d7c441995dadafc709ccfd76ce95755baf
Sapitorico <renzoyaques@gmail.com>
2023-02-24 17:08:58 -0300

    Create a seco class that saves data in jsons files

---
+++ models/engine/file_storage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This module contains the FileStorage class"""
+import json
+import os.path
+
+
+class FileStorage():
+    """
+    Private class attributes:
+        __file_path: string -Route to the JSON file (for example: file.json)
+        __objects: Dictionary: Vacuum
+    """
+    def __init__(self):
+        self.__file_path = "file.json"
+        self.__objects = {}
+
+    """
+    Public instance methods:
+        all(self): returns the dictionary __objects
+        new(self, obj): sets in __objects the obj with key <obj class name>.id
+        save(self): serializes __objects to the JSON file (path: __file_path)
+        reload(self): deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists ; otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)
+        create file json
+    """
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj.to_dict()
+
+    def save(self):
+        with open(self.__file_path, "w") as f:
+            json.dump(self.__objects, f)
+
+    def reload(self):
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                self.__objects = json.load(f)
+        else:
+            with open(self.__file_path, "w") as f:
+                json.dump(self.__objects, f)
