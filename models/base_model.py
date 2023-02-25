1e6ab419c32ce875a403edcd896aea6e41f6208c
Sapitorico <renzoyaques@gmail.com>
2023-02-25 19:06:57 -0300

    update

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,6 @@ from datetime import datetime
 from models import storage
 
 
-
 class BaseModel():
     """ Public instance attribute:
     * ID: assign with an UUID -String when an instance is created
@@ -31,17 +30,17 @@ class BaseModel():
                 if it’s a new instance (not from a dictionary representation), add a call to the method new(self) on storage
         """
         """ Now to get the current date and time """
-        self.updated_at = datetime.now()
-        self.created_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
             storage.new(self)
+
 
     """ Public instance methods:
     Save: Update the Datetime
