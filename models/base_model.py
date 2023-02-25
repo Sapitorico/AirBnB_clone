0d5ff471364d9e02bd5d5ff7d021466de0ec4aa5
Sapitorico <renzoyaques@gmail.com>
2023-02-25 14:54:43 -0300

    update a base models, order of id creation

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,10 @@
 """ this a parent class, it is in charge of creating your data model
 manage (create, update, destroy, etc.)
 objects through a console/command interpreter"""
-from uuid import uuid4
+import uuid
 from datetime import datetime
 from models.__init__ import storage
+
 
 
 class BaseModel():
@@ -31,15 +32,17 @@ class BaseModel():
         """
         """ Now to get the current date and time """
         self.updated_at = datetime.now()
+        self.created_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+                if key == id:
+                    self.id = value
         else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
+            self.id = str(uuid.uuid4())
             storage.new(self)
 
     """ Public instance methods:
@@ -63,14 +66,14 @@ class BaseModel():
             attributes of the established inst usual
         CREATED_AT and UPDATED_AT must become an ISO format object
         """
-        my_dict = []
+        my_dict = dict(self.__dict__)
         for key, value in self.__dict__.items():
             if key == "created_at" or key == "updated_at":
-                my_dict.append((key, value.isoformat()))
+                my_dict[key] = value.isoformat()
             else:
-                my_dict.append((key, value))
-        my_dict.append(("__class__", self.__class__.__name__))
-        return dict(my_dict)
+                my_dict[key] = value
+        my_dict['__class__'] = self.__class__.__name__
+        return my_dict
 
     """ Representation of classes like strings """
     def __str__(self):
