09291d85b67199ac32f9b491208d3e27b79b7c27
Sapitorico <renzoyaques@gmail.com>
2023-02-25 18:54:07 -0300

    possible fix a problem from created a id object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,7 +28,8 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[key] = obj.to_dict()
 
     def save(self):
         with open(self.__file_path, "w", encoding='utf-8') as f:
